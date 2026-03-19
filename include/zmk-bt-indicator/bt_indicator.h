52dd46b938f19f40704e0dfcc02cbb0960d2077a
zael99 <bdinelt@gmail.com>
2026-03-19 12:36:31 -0400

    Update bt_indicator.h

--- include/zmk-bt-indicator/bt_indicator.h
+++ include/zmk-bt-indicator/bt_indicator.h
@@ -1,2 +1,2 @@
-#DEFINE ACTIVE_LED_COLOUR {.h = 0, .s = 0, .b = 0}
-#DEFINE INACTIVE_LED_COLOUR {.h = 240, .s = 100, .b = 50}
+#define ACTIVE_LED_COLOUR {.h = 0, .s = 0, .b = 0}
+#define INACTIVE_LED_COLOUR {.h = 240, .s = 100, .b = 50}
