86de9fb0da9318316205069783186c1ea224215b
zael99 <5832951+zael99@users.noreply.github.com>
2026-04-01 11:49:43 -0400

    Update bt_indicator.h

--- include/dt-bindings/zmk/bt_indicator.h
+++ include/dt-bindings/zmk/bt_indicator.h
@@ -2,12 +2,8 @@
 #pragma once
 
 /* ====== Constant Definitions ====== */
-#define ACTIVE_LED_COLOUR {.h = 0, .s = 0, .b = 0}
-#define INACTIVE_LED_COLOUR {.h = 240, .s = 100, .b = 50}
-
-#define HUE_MAX 360
-#define SAT_MAX 100
-#define BRT_MAX 100
+#define ACTIVE_LED_COLOR {.h = 0, .s = 0, .b = 0}
+#define INACTIVE_LED_COLOR {.h = 240, .s = 100, .b = 50}
 /* ====== Constant Definitions ====== */
 
 /* ====== Struct Definitions ====== */
